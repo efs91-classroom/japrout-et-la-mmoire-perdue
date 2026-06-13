bf4f338b78a555c951fa14584f1bface75f94744
efs91-classroom <benoit@efs91.fr>
2026-06-13 12:49:19 +0200

    merge 13 06 2026 1249

--- main.ts
+++ main.ts
@@ -1,4 +1,32 @@
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    Hero.setImage(assets.image`Hero epée haute drotie`)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Hero.setImage(assets.image`hero epee milieu droite`)
+    pause(50)
+    Hero.setImage(assets.image`Hero epee droite basse`)
+})
+let Hero: Sprite = null
+let narateur = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+narateur.sayText("Vous êtes Japrout, lors de la guerre de Zamos vous vous êtes fait voler votre mémoire. Derkille LeFort (Le roi de Zamos) vous dépose Garuma une citée éloignée de toutes villes alentour. Il a séparé votre mémoire en petit fraguement qu' il a donner à des gardes royaux.", 20000, true)
 tiles.setCurrentTilemap(tilemap`niveau 1`)
-let mySprite = sprites.create(assets.image`Moob_1`, SpriteKind.Player)
-controller.moveSprite(mySprite)
-scene.cameraFollowSprite(mySprite)
+Hero = sprites.create(assets.image`Hero epée haute drotie`, SpriteKind.Player)
+scene.cameraFollowSprite(Hero)
+controller.moveSprite(Hero)
