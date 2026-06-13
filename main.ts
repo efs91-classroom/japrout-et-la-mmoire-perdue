95f5f2a0b34c7aeeeed556863b88a5031d276840
efs91-classroom <benoit@efs91.fr>
2026-06-13 12:05:12 +0200

    carte ethan

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+tiles.setCurrentTilemap(tilemap`niveau 1`)
+let mySprite = sprites.create(assets.image`Moob_1`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
